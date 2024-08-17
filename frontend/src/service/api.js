f18cddf9d1007b047c10bde7a64b1c9de794b512
padakitanmay <padakitanmay@gmail.com>
2024-08-17 07:52:21 +0530

    env

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URI = "http://localhost:5000";
+const API_URI = import.meta.env.VITE_APP_BASE_URL;
 
 // Uploads a file to the server
 export const uploadFile = async (data) => {
